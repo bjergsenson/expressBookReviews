c783e838acd5e926969562db114a634865b25cc1
bjergsenson <malondonoc@icloud.com>
2023-12-07 10:14:05 -0500

    Add general routes and update register and login methods

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: { "isbn": "0385474547", "author": "Chinua Achebe", "title": "Things Fall Apart", "reviews": { 'Jhon Doe': 'This book is fantastic! I really liked it a lot' } },
+      2: { "isbn": "0517092913", "author": "Hans Christian Andersen", "title": "Fairy tales", "reviews": {} },
+      3: { "isbn": "0451208633", "author": "Dante Alighieri", "title": "The Divine Comedy", "reviews": {} },
+      4: { "isbn": "0141026282", "author": "Unknown", "title": "The Epic Of Gilgamesh", "reviews": {} },
+      5: { "isbn": "0393319002", "author": "Unknown", "title": "The Book Of Job", "reviews": {} },
+      6: { "isbn": "0307948991", "author": "Unknown", "title": "One Thousand and One Nights", "reviews": {} },
+      7: { "isbn": "0140447699", "author": "Unknown", "title": "Nj\u00e1l's Saga", "reviews": {} },
+      8: { "isbn": "0141439518", "author": "Jane Austen", "title": "Pride and Prejudice", "reviews": {} },
+      9: { "isbn": "2253085799", "author": "Honor\u00e9 de Balzac", "title": "Le P\u00e8re Goriot", "reviews": {} },
+      10: { "isbn": "0375400702", "author": "Samuel Beckett", "title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
 }
 
-module.exports=books;
+module.exports = books;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,39 +5,120 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+const normalizeString = (str) => str.toLowerCase().replace(/[\u200B-\u200D\uFEFF]/g, '').replace(/\s+/g, '').trim();
+
+public_users.post("/register", (req, res) => {
+
 });
 
+
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
+  //Checks if there are any books
+  try {
+    if (!books || books.length === 0) {
+      return res.status(404).json({ message: "No books found" });
+    }
+    // Retrieve all books
+    res.status(200).json(books);
+  } catch (error) {
+    console.error("Error retrieving books:", error);
+    res.status(500).json({ message: "Internal server error" });
+  };
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
+public_users.get('/isbn/:isbn', function (req, res) {
+  try {
+    const requestedISBN = req.params.isbn;
+
+    // Using Object.values to get an array of books and filter the book by ISBN
+    const matchingBooks = Object.values(books).filter(book => book.isbn === requestedISBN);
+
+    if (matchingBooks.length === 0) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    // Assuming there's only one book with a given ISBN, use the first item in the filtered array
+    const book = matchingBooks[0];
+
+    res.status(200).json(book);
+  } catch (error) {
+    console.error("Error retrieving book by ISBN:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+
+  try {
+    const requestedAuthor = normalizeString(req.params.author);
+
+    // Using Object.values to get an array of books and filter the books by author
+    const matchingBooks = Object.values(books).filter(book => {
+      const authorName = normalizeString(book.author);
+      return authorName.includes(requestedAuthor);
+    });
+
+    if (matchingBooks.length === 0) {
+      return res.status(404).json({ message: "No books found for the given author" });
+    }
+
+    res.status(200).json(matchingBooks);
+  } catch (error) {
+    console.error("Error retrieving books by author:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title', function (req, res) {
+  try {
+    const requestedTitle = normalizeString(req.params.title);
+
+    // Using Object.values to get an array of books and filter the books by title
+    const matchingBooks = Object.values(books).filter(book => {
+      const bookTitle = normalizeString(book.title);
+      return bookTitle.includes(requestedTitle);
+    });
+
+    if (matchingBooks.length === 0) {
+      return res.status(404).json({ message: "No books found for the given title" });
+    }
+
+    res.status(200).json(matchingBooks);
+  } catch (error) {
+    console.error("Error retrieving books by title:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/review/:isbn', function (req, res) {
+  try {
+    const requestedISBN = req.params.isbn;
+    const matchingBooks = Object.values(books).filter(book => book.isbn === requestedISBN);
+
+    // Check if the requested ISBN exists in the books database
+    if (matchingBooks.length === 0) {
+      return res.status(404).json({ message: "No book found for the given ISBN" });
+    }
+
+    // Retrieve reviews for the specified book by ISBN
+    const bookReviews = matchingBooks[0] || {};
+    console.log(bookReviews.reviews)
+
+    if (Object.keys(bookReviews).length === 0) {
+      return res.status(404).json({ message: "No reviews found for the given ISBN" });
+    }
+
+    res.status(200).json(bookReviews.reviews);
+  } catch (error) {
+    console.error("Error retrieving reviews by ISBN:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 module.exports.general = public_users;
